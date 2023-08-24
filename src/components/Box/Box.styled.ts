bc2688d626d90d4678143f88acc9090f7b0991d5
RichEwin <richardewinlambert@gmail.com>
2023-08-24 14:16:20 +0200

    fix: made justify-content on box default to none

--- src/components/Box/Box.styled.ts
+++ src/components/Box/Box.styled.ts
@@ -11,8 +11,7 @@ interface StyledBoxProps {
 export const StyledBox = styled.div<StyledBoxProps>`
   display: ${(props) => (props.display ? props.display : "flex")};
   align-items: ${(props) => (props.alignItems ? props.alignItems : "center")};
-  justify-content: ${(props) =>
-    props.justifyContent ? props.justifyContent : "center"};
+  justify-content: ${(props) => props.justifyContent && props.justifyContent};
   gap: ${(props) => (props.gap ? props.gap : "0")};
   background-color: ${(props) =>
     props.backgroundColor ? props.backgroundColor : "transparent"};
